9af1c032943f0955bcd7c878be2289ef2ee8bb17
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-04-01 23:18:24 -0700

    add missing test script (fix build)

---
+++ tests/run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+# File: run-tests.sh
+# Author: Yuxin Wu <ppwwyyxxc@gmail.com>
+
+DIR=$(dirname $0)
+cd $DIR
+
+export TF_CPP_MIN_LOG_LEVEL=2
+python -m unittest discover -v
+cd ..
+python -m tensorpack.models._test
