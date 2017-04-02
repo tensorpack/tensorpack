502c461d21a58b44b868ce0f5932834bd50398e6
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-04-02 10:25:25 -0700

    remove layer tests for travis

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -8,4 +8,5 @@ cd $DIR
 export TF_CPP_MIN_LOG_LEVEL=2
 python -m unittest discover -v
 cd ..
-python -m tensorpack.models._test
+# python -m tensorpack.models._test
+# segfault for no reason (https://travis-ci.org/ppwwyyxx/tensorpack/jobs/217702985)
