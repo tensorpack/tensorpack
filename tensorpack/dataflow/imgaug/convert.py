8a4d259df272a65f95bacf233dc8654c68f5f54f
haamoon <amirreza@gatech.edu>
2017-07-19 07:54:22 -0700

    add identity coordinate mapping to ToUint8 and ToFloat32 augmentors (#339)
    * add identity coordinate mapping to ToUint8 and ToFloat32 augmentors

--- tensorpack/dataflow/imgaug/convert.py
+++ tensorpack/dataflow/imgaug/convert.py
@@ -45,10 +45,10 @@ class Grayscale(ColorSpace):
 class ToUint8(MapImage):
     """ Convert image to uint8. Useful to reduce communication overhead. """
     def __init__(self):
-        super(ToUint8, self).__init__(lambda x: np.clip(x, 0, 255).astype(np.uint8))
+        super(ToUint8, self).__init__(lambda x: np.clip(x, 0, 255).astype(np.uint8), lambda x: x)
 
 
 class ToFloat32(MapImage):
     """ Convert image to float32, may increase quality of the augmentor. """
     def __init__(self):
-        super(ToFloat32, self).__init__(lambda x: x.astype(np.float32))
+        super(ToFloat32, self).__init__(lambda x: x.astype(np.float32), lambda x: x)
