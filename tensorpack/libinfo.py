e93261053956dbafc94ba1ecfb9969e0f9cde4dc
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-07-31 23:44:39 -0700

    fix build

--- tensorpack/libinfo.py
+++ tensorpack/libinfo.py
@@ -1,15 +1,11 @@
 
 import os
-from .utils import logger
 
 # issue#7378 may happen with custom opencv. It doesn't hurt to disable opencl
 os.environ['OPENCV_OPENCL_RUNTIME'] = ''
 try:
     # issue#1924 may happen on old systems
     import cv2  # noqa
-except ImportError:
-    pass
-else:
     if int(cv2.__version__.split('.')[0]) == 3:
         cv2.ocl.setUseOpenCL(False)
     # check if cv is built with cuda
@@ -19,9 +15,11 @@ else:
             answer = line.split()[-1].lower()
             if answer == 'yes':
                 # issue#1197
-                logger.warn("OpenCV is built with CUDA support. "
-                            "This may cause slow initialization or sometimes segfault with TensorFlow.")
+                print("OpenCV is built with CUDA support. "
+                      "This may cause slow initialization or sometimes segfault with TensorFlow.")
             break
+except ImportError, TypeError:
+    pass
 
 os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'  # issue#9339
 os.environ['TF_AUTOTUNE_THRESHOLD'] = '3'   # use more warm-up
