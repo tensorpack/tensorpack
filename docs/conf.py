115f207cce0bebd7b001a4fecd49133c8cbaff5c
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-08-16 22:36:47 -0700

    bugfix in toc grouping. add GA for docs

--- docs/conf.py
+++ docs/conf.py
@@ -160,7 +160,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = { }
+html_theme_options = { 'analytics_id': 'UA-42349716-2' }
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
