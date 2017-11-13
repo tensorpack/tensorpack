d3d368ffe2e6aa50be09e5f10e0b61b661c7688e
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-11-14 00:47:02 +0800

    [CycleGAN] no soft label (#331)

--- examples/GAN/CycleGAN.py
+++ examples/GAN/CycleGAN.py
@@ -130,11 +130,11 @@ class Model(GANModelDesc):
                     B_dis_fake = self.discriminator(AB)
 
         def LSGAN_losses(real, fake):
-            d_real = tf.reduce_mean(tf.squared_difference(real, 0.9), name='d_real')
+            d_real = tf.reduce_mean(tf.squared_difference(real, 1), name='d_real')
             d_fake = tf.reduce_mean(tf.square(fake), name='d_fake')
             d_loss = tf.multiply(d_real + d_fake, 0.5, name='d_loss')
 
-            g_loss = tf.reduce_mean(tf.squared_difference(fake, 0.9), name='g_loss')
+            g_loss = tf.reduce_mean(tf.squared_difference(fake, 1), name='g_loss')
             add_moving_summary(g_loss, d_loss)
             return g_loss, d_loss
 
