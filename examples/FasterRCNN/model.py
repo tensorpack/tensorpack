[FasterRCNN] fix bug in gradient propagation

examples/FasterRCNN/model.py
@@ -292,7 +292,7 @@ def sample_fast_rcnn_targets(boxes, gt_boxes, gt_labels):
     ret_labels = tf.concat(
         [tf.gather(gt_labels, fg_inds_wrt_gt),
          tf.zeros_like(bg_inds, dtype=tf.int64)], axis=0, name='sampled_labels')
-    return ret_boxes, tf.stop_gradient(ret_labels), fg_inds_wrt_gt
+    return tf.stop_gradient(ret_boxes), tf.stop_gradient(ret_labels), fg_inds_wrt_gt
 
 
 @under_name_scope()