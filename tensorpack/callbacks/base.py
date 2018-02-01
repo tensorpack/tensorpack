b1c5766d7d657b1d7915bf95d08e2612aa491229
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-01-31 23:23:00 -0800

    set_chief_only in callbacks.

--- tensorpack/callbacks/base.py
+++ tensorpack/callbacks/base.py
@@ -201,6 +201,13 @@ class Callback(object):
     def chief_only(self, v):
         self._chief_only = v
 
+    def set_chief_only(self, v=True):
+        """
+        Set chief_only property, and returns the callback itself.
+        """
+        self._chief_only = v
+        return self
+
     def __str__(self):
         return type(self).__name__
 
