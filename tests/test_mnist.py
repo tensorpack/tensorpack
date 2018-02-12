bba7a9f56a282d1cd87deeee12067de449c281ee
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-02-12 02:59:29 -0800

    fix build

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -5,7 +5,7 @@ class MnistTest(TestPythonScript):
 
     @property
     def script(self):
-        return '../examples/mnist-convnet.py'
+        return '../examples/basics/mnist-convnet.py'
 
     def test(self):
         self.assertSurvive(self.script, args=None)
