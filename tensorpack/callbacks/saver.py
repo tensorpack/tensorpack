b17a098d356bbc83b7baba64db328532127480f1
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-07-15 18:25:28 -0700

    fix build

--- tensorpack/callbacks/saver.py
+++ tensorpack/callbacks/saver.py
@@ -73,8 +73,7 @@ class ModelSaver(Callback):
         self.saver.export_meta_graph(
             os.path.join(self.checkpoint_dir,
                          'graph-{}.meta'.format(time)),
-            collection_list=self.graph.get_all_collection_keys(),
-            clear_extraneous_savers=True)
+            collection_list=self.graph.get_all_collection_keys())
 
     def _trigger(self):
         try:
