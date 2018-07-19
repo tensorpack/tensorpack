7e022012ae90f3014f27d9aae7b8f4efb33295a9
PatWie <mail@patwie.com>
2018-07-19 09:16:46 +0200

    cosmetic changes

--- examples/CaffeModels/load-vgg19.py
+++ examples/CaffeModels/load-vgg19.py
@@ -17,6 +17,8 @@ enable_argscope_for_module(tf.layers)
 
 
 def tower_func(image):
+    is_training = get_current_tower_context().is_training
+
     with argscope([tf.layers.conv2d], kernel_size=3, activation=tf.nn.relu, padding='same'):
         x = image
         x = tf.layers.conv2d(x, 64, name='conv1_1')
@@ -25,31 +27,31 @@ def tower_func(image):
 
         x = tf.layers.conv2d(x, 128, name='conv2_1')
         x = tf.layers.conv2d(x, 128, name='conv2_2')
-        x = tf.layers.max_pooling2d(x, 2, 2, name='pool1')
+        x = tf.layers.max_pooling2d(x, 2, 2, name='pool2')
 
         x = tf.layers.conv2d(x, 256, name='conv3_1')
         x = tf.layers.conv2d(x, 256, name='conv3_2')
         x = tf.layers.conv2d(x, 256, name='conv3_3')
         x = tf.layers.conv2d(x, 256, name='conv3_4')
-        x = tf.layers.max_pooling2d(x, 2, 2, name='pool1')
+        x = tf.layers.max_pooling2d(x, 2, 2, name='pool3')
 
         x = tf.layers.conv2d(x, 512, name='conv4_1')
         x = tf.layers.conv2d(x, 512, name='conv4_2')
         x = tf.layers.conv2d(x, 512, name='conv4_3')
         x = tf.layers.conv2d(x, 512, name='conv4_4')
-        x = tf.layers.max_pooling2d(x, 2, 2, name='pool1')
+        x = tf.layers.max_pooling2d(x, 2, 2, name='pool4')
 
         x = tf.layers.conv2d(x, 512, name='conv5_1')
         x = tf.layers.conv2d(x, 512, name='conv5_2')
         x = tf.layers.conv2d(x, 512, name='conv5_3')
         x = tf.layers.conv2d(x, 512, name='conv5_4')
-        x = tf.layers.max_pooling2d(x, 2, 2, name='pool1')
+        x = tf.layers.max_pooling2d(x, 2, 2, name='pool5')
         x = tf.layers.flatten(x, name='flatten')
 
         x = tf.layers.dense(x, 4096, activation=tf.nn.relu, name='fc6')
-        x = tf.layers.dropout(x, rate=0.5, name='drop0')
+        x = tf.layers.dropout(x, rate=0.5, name='drop0', training=is_training)
         x = tf.layers.dense(x, 4096, activation=tf.nn.relu, name='fc7')
-        x = tf.layers.dropout(x, rate=0.5, name='drop1')
+        x = tf.layers.dropout(x, rate=0.5, name='drop1', training=is_training)
         logits = tf.layers.dense(x, 1000, activation=tf.identity, name='fc8')
 
     tf.nn.softmax(logits, name='prob')
