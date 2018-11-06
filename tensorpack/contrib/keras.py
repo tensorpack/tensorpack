f5ba2692e7d61690fa5ff5015cfbf4f74bf1d458
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-11-06 21:09:13 +0800

    fix #973

--- tensorpack/contrib/keras.py
+++ tensorpack/contrib/keras.py
@@ -259,10 +259,15 @@ class KerasModel(object):
         """
         Args:
             validation_data (DataFlow or InputSource): to be used for inference.
+                The inference callback is added as the first in the callback list.
+                If you need to use it in a different order, please write it in the callback list manually.
             kwargs: same as `self.trainer.train_with_defaults`.
         """
         callbacks = kwargs.pop('callbacks', [])
         if validation_data is not None:
-            callbacks.append(InferenceRunner(
+            # There is no way to guess where users want this callback. So we have to choose one.
+            # MinSaver may need results from this callback,
+            # so we put this callback at first.
+            callbacks.insert(0, InferenceRunner(
                 validation_data, ScalarStats(self._stats_to_inference)))
         self.trainer.train_with_defaults(callbacks=callbacks, **kwargs)
