d52d2889a16597845be1a0c10f393152a85b9648
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2019-09-29 16:22:09 -0700

    sotabench: install missing deps

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -v
+
 . /workspace/venv/bin/activate
 
 pip install -e .
+pip install tensorflow-gpu==1.14.0
+pip install opencv-python scipy
 
+echo "Extracting ..."
 cd ./.data/vision/coco
 python -c 'import zipfile; zipfile.ZipFile("annotations_trainval2017.zip").extractall()'
 python -c 'import zipfile; zipfile.ZipFile("val2017.zip").extractall()'
